aa4da7cfaabb3f9c24f8ad15fe014992e3542481
Mohammed Mozammel <72930299+mozammel787@users.noreply.github.com>
2025-07-30 20:17:20 +0600

    Update index.js

--- index.js
+++ index.js
@@ -214,6 +214,12 @@ async function run() {
             const product = await productsCollection.find(query).toArray()
             res.send(product)
         })
+        app.get('/product/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await productsCollection.updateOne(query)
+            res.send(result)
+        })
         app.delete('/product/:id', verifyJWT, async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
