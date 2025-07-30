32c04d8620549a3600360fd26a484f41227f1f45
Mohammed Mozammel <72930299+mozammel787@users.noreply.github.com>
2025-07-30 20:24:38 +0600

    Update index.js

--- index.js
+++ index.js
@@ -214,7 +214,7 @@ async function run() {
             const product = await productsCollection.find(query).toArray()
             res.send(product)
         })
-        app.get('/product/:id',  async (req, res) => {
+        app.get('/product/:id', verifyJWT,  async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const result = await productsCollection.findOne(query)
